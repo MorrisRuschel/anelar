7e49075516316d063e1dc23764900556620cf01a
Morris Ruschel <morrisruschel@gmail.com>
2022-04-24 21:39:18 -0300

    Update index.js

--- workers/feed/index.js
+++ workers/feed/index.js
@@ -1,3 +1,63 @@
+async function do_request( url, options )
+{
+	const https = require( 'https' );
+
+	/*const options =
+	{
+		hostname:	'www.domain.com.br',
+		port:		443,
+		path:		'/',
+		method: 	'GET',
+		headers:
+		{
+			'Content-Type': 	'application/json'
+		}
+	};*/
+
+	return new Promise((resolve, reject) => {
+	console.log('t1');
+		const req = https.request
+		(	
+			url,
+			options,
+			(res) =>
+			{
+				res.setEncoding( 'utf8' );
+					console.log('t2');
+
+				let response2 = '';
+
+				res.on
+				(
+					'data',
+					( chunk ) =>
+					{
+						response2 += chunk;
+					}
+				);
+				res.on
+				(
+					'end',
+					() =>
+					{
+						resolve( response2 );
+					}
+				);
+
+			}
+		).on
+		(
+			'error',
+			(error) =>
+			{
+				console.error( error );
+			}
+		);
+
+		req.end();
+	});
+};
+
 exports.handler = async (event) => {
 
 	let response = {
@@ -5,44 +65,21 @@ exports.handler = async (event) => {
 		body: JSON.stringify('Error #01. Internal error.'),
 	};
 
-	const https = require('https');
-
 	const server_api = 'https://api.nitrado.net';
 	const server_id = process.env.SERVER_ID;
 	const account_id = process.env.ACCOUNT_ID;
 
+	let url = server_api + '/services/' + server_id + '/gameservers/file_server/list?dir=/games/' + account_id + '/noftp/dayzxb/';
 	let options = {
 		headers:
 		{
 			Authorization: process.env.ACCOUNT_TOKEN
 		}
 	};
+	
+	let request = await do_request( url, options );
 
-	const request = https.get
-	(
-		server_api + '/services/' + server_id + '/gameservers/file_server/list?dir=/games/' + account_id + '/noftp/dayzxb/',
-		options,
-		(res) =>
-		{
-			console.log( res );
-
-			res.on
-			(
-				'data',
-				(d) =>
-				{
-					process.stdout.write(d);
-				}
-			);
-		}
-	).on( 'error', (error) => {
-		console.log( error );
-
-		response = {
-			statusCode: 200,
-			body: JSON.stringify('Error #02. Internal error.'),
-		};
-	});
-
-    return response;
+	console.log(request);
+	
+	return response;
 };
