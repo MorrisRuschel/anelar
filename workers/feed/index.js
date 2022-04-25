cb61fc3a6f5ff3c64357b3d0887f68b8a7cc33a1
Morris Ruschel <morrisruschel@gmail.com>
2022-04-24 21:01:22 -0300

    Update index.js

--- workers/feed/index.js
+++ workers/feed/index.js
@@ -1,10 +1,10 @@
 exports.handler = async (event) => {
 
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify('Error #01. Internal error.'),
-    };
-	
+	let response = {
+		statusCode: 200,
+		body: JSON.stringify('Error #01. Internal error.'),
+	};
+
 	const https = require('https');
 
 	const server_api = 'https://api.nitrado.net';
@@ -17,7 +17,7 @@ exports.handler = async (event) => {
 			Authorization: process.env.ACCOUNT_TOKEN
 		}
 	};
-	
+
 	const request = https.get
 	(
 		server_api + '/services/' + server_id + '/gameservers/file_server/list?dir=/games/' + account_id + '/noftp/dayzxb/',
@@ -44,9 +44,5 @@ exports.handler = async (event) => {
 		};
 	});
 
-    response = {
-        statusCode: 200,
-        body: JSON.stringify('Success #01. Internal success.'),
-    };
     return response;
 };
